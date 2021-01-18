85431f4476b48ab3d17f50c75adcb6dd0589cfee
Cuda Chen <clh960524@gmail.com>
2021-01-18 22:39:44 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Zergling />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Zergling from './components/Zergling.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Zergling
   }
 }
 </script>
